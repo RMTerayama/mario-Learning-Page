a1996c2f7e5cc5fedd14e490bb8b2bb3f33dd41e
Richard Masayuki Terayama <85762067+RMTerayama@users.noreply.github.com>
2023-01-22 19:46:45 -0400

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -5,16 +5,18 @@ const video = document.getElementById("video");
 const modal = document.querySelector(".modal");
 const linkDoVideo = video.src;
 
+const botaoDetalhe = document.querySelector(".botao-detalhe");
+const detalhe = document.querySelector(".modal-detalhes");
+const botaoFecharDetalhe = document.querySelector(".fechar-detalhes");
 
 function loading(){
 	preLoad.style.display='none';
 	console.log("funcao preload");
   }
+
 const pageLoad= setTimeout(()=>{
    loading();
 },1500)
-
-
 
 function alternarModal(){
 	modal.classList.toggle("aberto");
@@ -29,4 +31,18 @@ botaoTrailer.addEventListener("click", () => {
 botaoFecharModal.addEventListener("click", () => {
 	alternarModal();
 	video.setAttribute("src", "");
+});
+
+
+function alternarDetalhe(){
+	detalhe.classList.toggle("aberto");
+	
+}
+
+botaoFecharDetalhe.addEventListener("click", () => {
+	alternarDetalhe();
+});
+
+botaoDetalhe.addEventListener("click", () => {
+	alternarDetalhe();
 });
