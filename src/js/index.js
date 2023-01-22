77737b63688cd53a399f9031ccca6f01ac91fc2a
Richard Masayuki Terayama <85762067+RMTerayama@users.noreply.github.com>
2023-01-21 23:46:44 -0400

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,24 @@
+const preLoad = document.querySelector('.box-load');
 const botaoTrailer = document.querySelector(".botao-trailer");
 const botaoFecharModal = document.querySelector(".fechar-modal");
 const video = document.getElementById("video");
 const modal = document.querySelector(".modal");
 const linkDoVideo = video.src;
 
+
+function loading(){
+	preLoad.style.display='none';
+	console.log("funcao preload");
+  }
+const pageLoad= setTimeout(()=>{
+   loading();
+},1500)
+
+
+
 function alternarModal(){
 	modal.classList.toggle("aberto");
+
 }
 
 botaoTrailer.addEventListener("click", () => {
